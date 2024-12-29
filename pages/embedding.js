b0db489e07ff88468b48093cfd13804714d84cce
Oussama Ben Saied <ben.saied@proton.me>
2024-12-29 11:52:46 +0100

    Feat: setup the first step selecting embedding method in the 3rd pipeline

--- pages/embedding.js
+++ pages/embedding.js
@@ -1,7 +1,14 @@
 "use client";
 import { useState, useEffect, useRef, useMemo } from "react";
 import { useRouter } from "next/router";
-
+import {
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Typography,
+  Box,
+} from "@mui/material";
 import styles from "../src/app/css/embedding.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
@@ -22,7 +29,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Chart } from "chart.js/auto";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
-import { Box } from "@mui/material";
+
 // import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 // import { useDemoData } from "@mui/x-data-grid-generator";
 
@@ -37,6 +44,37 @@ const PipilineEmbedding = () => {
   // Pipeline Stage Status (1-2)
   const [currentStage, setCurrentStage] = useState(1);
   // Step 1 - Select Method
+  const [selectedMethod, setSelectedMethod] = useState("");
+  const [description, setDescription] = useState("");
+  // Handle Method Switching
+  const handleMethodChange = (event) => {
+    const method = event.target.value;
+    setSelectedMethod(method);
+    switch (method) {
+      case "camel":
+        setDescription(
+          "CAMeL Tools: Specialized models for dialects like Egyptian, Levantine, and Gulf."
+        );
+        break;
+      case "arabert":
+        setDescription(
+          "AraBERT: A fine-tuned BERT model designed for Arabic dialects."
+        );
+        break;
+      case "fasttext":
+        setDescription(
+          "FastText: Captures subword-level info, useful for slang and rare words."
+        );
+        break;
+      case "tfidf":
+        setDescription(
+          "TF-IDF: A traditional method for term importance and document similarity."
+        );
+        break;
+      default:
+        setDescription("");
+    }
+  };
 
   useEffect(() => {
     document.title = "ScrapeSense";
@@ -157,14 +195,53 @@ const PipilineEmbedding = () => {
           <>
             {currentStage === 1 ? (
               <>
-                <button
-                  title="Proceed to Step 2"
-                  className={styles.proceedButtonStep1}
-                  onClick={() => setCurrentStage(2)}
-                >
-                  {" "}
-                  <FontAwesomeIcon icon={faArrowRight} />
-                </button>
+                <div className={styles.selectMethodContainer}>
+                  <div className={styles.selectMethodTitle}>
+                    Select Emmbedding Method
+                  </div>
+                  <FormControl fullWidth variant="outlined">
+                    <InputLabel id="embedding-method-label">
+                      Choose a Method
+                    </InputLabel>
+                    <Select
+                      labelId="embedding-method-label"
+                      value={selectedMethod}
+                      onChange={handleMethodChange}
+                      label="Choose a Method"
+                    >
+                      <MenuItem value="camel">CAMeL Tools</MenuItem>
+                      <MenuItem value="arabert">AraBERT</MenuItem>
+                      <MenuItem value="fasttext">FastText</MenuItem>
+                      <MenuItem value="tfidf">TF-IDF</MenuItem>
+                    </Select>
+                  </FormControl>
+
+                  {/* Display the description under the dropdown */}
+                  {description && (
+                    <div className={styles.inlineContainer}>
+                      <FontAwesomeIcon
+                        icon={faCircleInfo}
+                        className={styles.iconPadding}
+                      />
+                      <Typography
+                        variant="body2"
+                        className={styles.descriptionText}
+                      >
+                        {description}
+                      </Typography>
+                    </div>
+                  )}
+                </div>
+                {selectedMethod && (
+                  <button
+                    title="Proceed to Step 2"
+                    className={styles.proceedButtonStep1}
+                    onClick={() => setCurrentStage(2)}
+                  >
+                    {" "}
+                    <FontAwesomeIcon icon={faArrowRight} />
+                  </button>
+                )}
               </>
             ) : currentStage === 2 ? (
               /* Second Step: Data Inspection */
